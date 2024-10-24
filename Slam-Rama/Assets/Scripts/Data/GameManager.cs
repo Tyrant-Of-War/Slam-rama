432e7db64a5fa1fc49e2e6af6c104027d82dd04f
IcyGambit <morgan.tuck@icloud.com>
2024-10-24 16:41:45 +0100

    0.0.3 LoserCards
    -Wrote the script for the PlayerDeath function
    -Edited the GameManager and PlayerData scripts accordingly to fit the playerdeath and losercard requirements
    -Will comment code changed at a later date
    -Attached scripts to player prefab, no issues so far
    -Code needs to be properly tested in another gamespace, works fine on my PC
    -TLDR: Keeps track of player lives (set at 3 on startup), left room for game function and player falling off, when ALL BUT 1 players have run out of lives, scene changes to losercards scene (need to add delay to it)
    
    Future changes to be made:
    
    -Functionality on LoserCards Scene
    -Forgot to include results scene, will change to accommodate when delay is sorted for text display saying "*Player Number* is the winner"
    -Needs further testing with multiple players

--- Slam-Rama/Assets/Scripts/Data/GameManager.cs
+++ Slam-Rama/Assets/Scripts/Data/GameManager.cs
@@ -1,55 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Windows;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] List<PlayerData> playerData = new List<PlayerData>();
-
     int playerCount;
-
     [SerializeField] LevelData levelData;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        if (playerData[0].playerY < levelData.killHeight)
+        for (int i = 0; i < playerData.Count; i++)
         {
-            Debug.Log("Player Kill");
+            if (playerData[i].playerY < levelData.killHeight)
+            {
+                PlayerDeathHandler(i);
+            }
         }
+        CheckRoundEnd();
     }
-
     public void playerSetup(UnityEngine.InputSystem.PlayerInput input)
     {
         playerCount = UnityEngine.InputSystem.PlayerInput.all.Count;
 
-        switch(playerCount)
+        switch (playerCount)
         {
             case 1:
-                input.GetComponent<Damage>().playerData = playerData[0];
-                input.GetComponent<Knockback>().playerData = playerData[0];
-                input.GetComponent<MeshRenderer>().material = playerData[0].playerMaterial;
+                AssignPlayerData(input, playerData[0]);
                 break;
             case 2:
-                input.GetComponent<Damage>().playerData = playerData[1];
-                input.GetComponent<Knockback>().playerData = playerData[1];
-                input.GetComponent<MeshRenderer>().material = playerData[1].playerMaterial;
+                AssignPlayerData(input, playerData[1]);
                 break;
             case 3:
-                input.GetComponent<Damage>().playerData = playerData[2];
-                input.GetComponent<Knockback>().playerData = playerData[2];
-                input.GetComponent<MeshRenderer>().material = playerData[2].playerMaterial;
+                AssignPlayerData(input, playerData[2]);
                 break;
             case 4:
-                input.GetComponent<Damage>().playerData = playerData[3];
-                input.GetComponent<Knockback>().playerData = playerData[3];
-                input.GetComponent<MeshRenderer>().material = playerData[3].playerMaterial;
+                AssignPlayerData(input, playerData[3]);
                 break;
         }
     }
+    private void AssignPlayerData(UnityEngine.InputSystem.PlayerInput input, PlayerData data)
+    {
+        input.GetComponent<Damage>().playerData = data;
+        input.GetComponent<Knockback>().playerData = data;
+        input.GetComponent<MeshRenderer>().material = data.playerMaterial;
+    }
+
+    private void PlayerDeathHandler(int playerIndex)
+    {
+        PlayerData currentPlayer = playerData[playerIndex];
+        currentPlayer.lives--;
+
+        if (currentPlayer.lives <= 0)
+        {
+            currentPlayer.lives = 0;
+        }
+    }
+    public void CheckRoundEnd()
+    {
+        int playersWithLives = 0;
+        PlayerData lastPlayerStanding = null;
+
+        foreach (PlayerData player in playerData)
+        {
+            if (player.lives > 0)
+            {
+                playersWithLives++;
+                lastPlayerStanding = player;
+            }
+        }
+
+        if (playersWithLives <= 1)
+        {
+            EndRound(lastPlayerStanding);
+        }
+    }
+
+
+    private void EndRound(PlayerData winner)
+    {
+        if (winner != null)
+        {
+           SceneManager.LoadScene("LoserCards");  
+        }
+    }
 }
+
+
