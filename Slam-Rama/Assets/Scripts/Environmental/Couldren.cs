04475367af584c284c70014c89d9ebd8f09ca507
Chefatron <binkybenjy@gmail.com>
2025-01-06 15:31:16 +0000

    0.1.1 Cauldron Environment Hazard
    - Added the environmental hazard for the cauldron level, three platforms have been chosen as platforms that will fall
    - Every 15 seconds a random one is chosen and will begin to shake to alert players, it will then sink into the cauldron staying there for 12 ish seconds, then coming back up

---
+++ Slam-Rama/Assets/Scripts/Environmental/Couldren.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Couldren : MonoBehaviour
+{
+    [SerializeField] Animator platforms;
+
+    float platformDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformDelay = 15f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (platformDelay > 0)
+        {
+            platformDelay = platformDelay - Time.deltaTime;
+        }
+        else
+        {
+            platforms.SetTrigger("Drop" + Random.Range(1, 4).ToString());
+
+            platformDelay = 15f;
+        }
+    }
+}
